f916dc9d92a10e9d3c040b1dc89117124425ef83
Pras1234 <parashantsharma8@gmail.com>
2025-06-19 22:44:03 +0530

    Committing New file

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -1,6 +1,18 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://www.google.com')
-    cy.title().should('eq','Google')
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.title().should('include','OrangeHRM')
+    .and('contain','HRM')
+
+
   })
+  it('passes', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.url().should('contain','demo')
+    cy.xpath("//a").should('have.length',5)
+
+
+
+  })
+
 })
