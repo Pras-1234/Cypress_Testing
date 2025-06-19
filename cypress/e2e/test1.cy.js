2501b2b7ccfdb80a26abb21b3a8498b16649b1e2
Pras1234 <parashantsharma8@gmail.com>
2025-06-19 23:22:47 +0530

    Committing New file

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -9,7 +9,7 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     cy.url().should('contain','demo')
-    cy.xpath("//a").should('have.length',5)
+    cy.xpath("//a").should('have.length',3)
 
 
 
